c2997e2f8c9bd8b4559380f3612a6aee1d567183
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-03-10 10:34:11 -0800

    Add files via upload

---
+++ apartment/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1686683.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 125030378, 125030386, 125030388, 125030402, 125030405, 125030412 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/125030371/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/125030372/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/125030370/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/125030375/1/basis.wasm.js', 'wasmUrl' : 'files/assets/125030376/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/125030374/1/basis.js', 'preload' : false},
+];
