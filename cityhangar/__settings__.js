1a86d3ffa10335a77c5b526ac1b7651b8b382b43
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-03-13 10:44:02 -0700

    Add files via upload

---
+++ cityhangar/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1688658.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 125303356, 125303364, 125303366, 125303380, 125303383, 125303390 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/125303353/1/basis.wasm.js', 'wasmUrl' : 'files/assets/125303354/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/125303352/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/125303349/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/125303350/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/125303348/1/ammo.js', 'preload' : true},
+];
