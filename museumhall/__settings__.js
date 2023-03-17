b2933740e3046ad3017139c0b96a91104e40b6bc
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-03-17 11:17:21 -0700

    Add files via upload

---
+++ museumhall/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1694842.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 126005932, 126005940, 126005942, 126005956, 126005959, 126005966 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/126005929/1/basis.wasm.js', 'wasmUrl' : 'files/assets/126005930/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/126005928/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/126005925/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/126005926/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/126005924/1/ammo.js', 'preload' : true},
+];
