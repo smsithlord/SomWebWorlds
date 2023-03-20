5d5c9789df3e77a68e2991e400b6a0bee3467253
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-03-20 14:54:25 -0700

    Add files via upload

---
+++ halltime/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1696504.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 126240990, 126240998, 126241000, 126241014, 126241017, 126241024 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/126240983/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/126240984/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/126240982/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/126240987/1/basis.wasm.js', 'wasmUrl' : 'files/assets/126240988/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/126240986/1/basis.js', 'preload' : false},
+];
