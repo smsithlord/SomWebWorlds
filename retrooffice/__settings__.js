350814946cb5bcc5142ba67ace7bd9e595b63b30
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-03-24 10:46:14 -0700

    Add files via upload

---
+++ retrooffice/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1701064.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 126882597, 126882605, 126882607, 126882621, 126882624, 126882631 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/126882594/1/basis.wasm.js', 'wasmUrl' : 'files/assets/126882595/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/126882593/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/126882590/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/126882591/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/126882589/1/ammo.js', 'preload' : true},
+];
