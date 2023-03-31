6724e26922facf44a238227aa612af1e923c9e41
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-03-31 12:44:38 -0700

    Add files via upload

---
+++ calmroom/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1707969.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 127815893, 127815901, 127815903, 127815917, 127815920, 127815927 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/127815886/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/127815887/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/127815885/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/127815890/1/basis.wasm.js', 'wasmUrl' : 'files/assets/127815891/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/127815889/1/basis.js', 'preload' : false},
+];
