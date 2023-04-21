ff46c383a12df38c668761a51b168a49ce547a54
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-04-21 11:36:03 -0700

    Add files via upload

---
+++ sargasso/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1720762.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 129604345, 129604371, 129604350, 129604351, 129604359, 129604356 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/129604368/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/129604357/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/129604343/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/129604365/1/basis.wasm.js', 'wasmUrl' : 'files/assets/129604369/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/129604346/1/basis.js', 'preload' : false},
+];
