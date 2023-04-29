c407ad0a52d8fa5127df79018793d05c4afd4e01
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-04-29 06:01:23 -0700

    Add files via upload

---
+++ webbaketests/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1731826.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 131715281, 131715289, 131715291, 131715305, 131715308, 131715315, 131715317, 131715318 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/131715274/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/131715275/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/131715273/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/131715278/1/basis.wasm.js', 'wasmUrl' : 'files/assets/131715279/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/131715277/1/basis.js', 'preload' : false},
+];
