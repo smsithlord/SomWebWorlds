7a71ce88052650217553333d463b7d2ffd806d40
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-05-27 05:05:41 -0700

    Add files via upload

---
+++ bakedtheaterroom/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1756588.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 136799427, 136799435, 136799437, 136799451, 136799454, 136799461, 136799463, 136799464 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/136799424/1/basis.wasm.js', 'wasmUrl' : 'files/assets/136799425/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/136799423/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/136799420/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/136799421/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/136799419/1/ammo.js', 'preload' : true},
+];
